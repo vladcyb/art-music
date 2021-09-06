13feb3589782f32ba3437b35f7fa890f045939f4
Vlad Tsibizov <vladtsibizov@gmail.com>
2021-09-06 23:25:15 +0300

    remove react-group-transition

--- src/pages/Info/Info.tsx
+++ src/pages/Info/Info.tsx
@@ -1,5 +1,4 @@
 import { BsSpeaker, GiDrum, GiGuitarHead, GiPianoKeys, GiViolin } from 'react-icons/all';
-import { CSSTransition } from 'react-transition-group';
 import { Col, Row } from 'antd';
 import { Fragment } from 'react';
 import './Info.scss';
@@ -31,55 +30,44 @@ const musicIcons = [
 export const Info = () => (
   <section className="info">
     <div className="info__wrapper">
-      <CSSTransition
-        className="info__title"
-        appear
-        in
-        timeout={0}
-      >
-        <h1>
+      <h1 className="info__title">
           Art-Music
-        </h1>
-      </CSSTransition>
-      <CSSTransition appear in timeout={0}>
-        <div className="info__content-wrapper">
-          <Row className="info__content"
-            wrap
-            // gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
-            justify="start"
-            align="middle"
-          >
-            <Col offset={3} className="info__logo-wrapper" sm={6}>
-              <img className="info__logo" src="/i/logo.svg" alt="логотоп Art-Music" />
-            </Col>
-            <Col sm={8}>
-              <p>
-                ООО &quot;Арт-Мьюзик&quot; занимается поставкой и арендой звукового и
-                светового оборудования, а также их монтажем. В том числе
-                мы предлагаем широкий выбор любых музыкальных инструментов
-                от разных брендов, среди которых Yamaha, Das Audio, Turbosound,
-                Fender, XLine и другие.
-              </p>
-              <p>
-                Также мы участвуем в государственных закупках по закону №44-ФЗ
-                и №223-ФЗ.
-              </p>
-            </Col>
-          </Row>
-        </div>
-      </CSSTransition>
+      </h1>
+      <div className="info__content-wrapper">
+        <Row className="info__content"
+          wrap
+          // gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
+          justify="start"
+          align="middle"
+        >
+          <Col offset={3} className="info__logo-wrapper" sm={6}>
+            <img className="info__logo" src="/i/logo.svg" alt="логотоп Art-Music" />
+          </Col>
+          <Col sm={10}>
+            <p>
+              ООО &quot;Арт-Мьюзик&quot; занимается поставкой и арендой звукового и
+              светового оборудования, а также их монтажем. В том числе
+              мы предлагаем широкий выбор любых музыкальных инструментов
+              от разных брендов, среди которых Yamaha, Das Audio, Turbosound,
+              Fender, XLine и другие.
+            </p>
+            <p>
+              Также мы участвуем в государственных закупках по закону №44-ФЗ
+              и №223-ФЗ.
+            </p>
+          </Col>
+        </Row>
+      </div>
     </div>
     <Row justify="center">
       <Col sm={18}>
-        <CSSTransition className="info__icons" appear in timeout={0}>
-          <div>
-            {musicIcons.map((item) => (
-              <Fragment key={item.id}>
-                {item.component}
-              </Fragment>
-            ))}
-          </div>
-        </CSSTransition>
+        <div className="info__icons">
+          {musicIcons.map((item) => (
+            <Fragment key={item.id}>
+              {item.component}
+            </Fragment>
+          ))}
+        </div>
       </Col>
     </Row>
   </section>
