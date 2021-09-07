c1dcc3641a4f6cef0fd899cd329908538f7a6691
Vlad Tsibizov <vladtsibizov@gmail.com>
2021-09-08 00:15:30 +0300

    remove fonts hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,22 @@
 import React from 'react';
-import { Layout, Spin } from 'antd';
+import { Layout } from 'antd';
 import { About, Brands, CallBack, Contacts, Info } from './pages';
 import { Navbar } from './components';
-import { useFonts } from './shared/hooks/useFonts';
 import { Payment } from './pages/Payment';
 import { Content } from 'antd/es/layout/layout';
 import './App.scss';
 
 
-export const App = () => {
-  // Loading fonts: "Kaisei HarunoUmi", "Montserrat"
-  const fontsLoaded = useFonts('https://fonts.googleapis.com/css2?family=Kaisei+HarunoUmi:wght@400;500;700&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap%22%20rel=%22stylesheet');
-
-  if (!fontsLoaded) {
-    return (
-      <Layout className="app app_loading">
-        <section className="app__preloader">
-          <Spin className="app__preloader-spin" size="large" />
-        </section>
-      </Layout>
-    );
-  }
-
-  return (
-    <Layout className="app">
-      <Navbar />
-      <Content>
-        <Info />
-        <Brands />
-        <About />
-        <CallBack />
-        <Payment />
-        <Contacts />
-      </Content>
-    </Layout>
-  );
-};
+export const App = () => (
+  <Layout className="app">
+    <Navbar />
+    <Content>
+      <Info />
+      <Brands />
+      <About />
+      <CallBack />
+      <Payment />
+      <Contacts />
+    </Content>
+  </Layout>
+);
