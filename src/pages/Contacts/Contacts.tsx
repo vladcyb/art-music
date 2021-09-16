db06d2f26334a05ab60460abc6d0acaf2072dfda
Vlad Tsibizov <vladtsibizov@gmail.com>
2021-09-17 02:48:44 +0300

    fix contacts

--- src/pages/Contacts/Contacts.tsx
+++ src/pages/Contacts/Contacts.tsx
@@ -6,25 +6,23 @@ export const Contacts = () => (
     <Col className="contacts" xs={24} xl={14}>
       <div className="card" id="contacts">
         <h2>Контакты</h2>
-        <div className="contacts__content">
-          <p>
-            <span>Адрес:</span>
-            {' '}
-            <a href="https://yandex.ru/maps/-/CCUm6LwtdC">
-            г. Воронеж, ул. Машиностроителей, д.&nbsp;82</a>
-          </p>
-          <p>
-            <span>Телефон:</span>
-            {' '}
-            <a className="contacts__phone" href="tel:+79204022299">+7 920 402-22-99</a>,{' '}
+        <p>
+          <span>Адрес:</span>
+          <a className="contacts__value" href="https://yandex.ru/maps/-/CCUm6LwtdC">
+            г. Воронеж, ул.&nbsp;Машиностроителей,&nbsp;д.&nbsp;82
+          </a>
+        </p>
+        <p>
+          <span>Телефон:</span>
+          <span className="contacts__value">
+            <a className="contacts__phone" href="tel:+79204022299">+7 920 402-22-99,</a>
             <a className="contacts__phone" href="tel:+79202170035">+7 920 217-00-35</a>
-          </p>
-          <p>
-            <span>Email:</span>
-            {' '}
-            <a href="mailto:vmb36@yandex.ru">vmb36@yandex.ru</a>
-          </p>
-        </div>
+          </span>
+        </p>
+        <p>
+          <span>Email:</span>
+          <a className="contacts__value" href="mailto:vmb36@yandex.ru">vmb36@yandex.ru</a>
+        </p>
       </div>
     </Col>
   </Row>
