f8c6c14bdd4ef8d48222768ddf1d543b855d31e9
Vlad Tsibizov <vladtsibizov@gmail.com>
2021-09-18 01:58:25 +0300

    add className for card that is not in viewport

--- src/components/MyCard/MyCard.tsx
+++ src/components/MyCard/MyCard.tsx
@@ -1,11 +1,25 @@
 import classNames from 'classnames';
-import { HTMLAttributes, PropsWithChildren } from 'react';
+import { HTMLAttributes, PropsWithChildren, useEffect, useRef, useState } from 'react';
+import { useScrollPosition } from '../../shared/hooks/useScrollPosition';
+import './MyCard.scss';
 
 interface IMyCard extends HTMLAttributes<HTMLDivElement> {}
 
 export const MyCard = ({ className, children, ...props }: PropsWithChildren<IMyCard>) => {
+  const scrollPosition = useScrollPosition(100);
+  const [isInViewport, setIsInViewport] = useState(false);
+
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    setIsInViewport((ref.current?.getBoundingClientRect().top || 0) < window.innerHeight);
+  }, [ref.current?.getBoundingClientRect().top, scrollPosition]);
+
   return (
-    <div className={classNames(className, 'card')} {...props}>
+    <div className={classNames(className, 'card', {
+      card_hidden: !isInViewport,
+      card_visible: isInViewport,
+    })} {...props} ref={ref}>
       {children}
     </div>
   );
