8d7dcb120b2686856442cdc0d26844e448a1309e
Vlad Tsibizov <vladtsibizov@gmail.com>
2021-10-30 21:59:34 +0300

    навбар

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -8,11 +8,11 @@ const { Link } = Anchor;
 export const Navbar = () => (
   <Header className="navbar">
     <Anchor bounds={200} targetOffset={84}>
-      <Link href="#brands" title="Бренды" />
-      <Link href="#rent" title="Аренда" />
-      <Link href="#stage" title="Одежда сцены" />
-      <Link href="#repair" title="Ремонт" />
-      <Link className="navbar__contacts" href="#contacts" title="Контакты" />
+      <Link className="navbar__item" href="#brands" title="Бренды" />
+      <Link className="navbar__item" href="#rent" title="Аренда" />
+      <Link className="navbar__item" href="#stage" title="Одежда сцены" />
+      <Link className="navbar__item" href="#repair" title="Ремонт" />
+      <Link className="navbar__item navbar__contacts" href="#contacts" title="Контакты" />
     </Anchor>
   </Header>
 );
