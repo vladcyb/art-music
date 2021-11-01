538dbec5528eb627f60223f2cc4557a33a8c2f49
Vlad Tsibizov <vladtsibizov@gmail.com>
2021-11-01 11:39:59 +0300

    fix callback

--- src/pages/CallBack/CallBack.tsx
+++ src/pages/CallBack/CallBack.tsx
@@ -1,7 +1,6 @@
-import { Button, Col, Form, Input, Row } from 'antd';
+import { Button, Form, Input } from 'antd';
 import MaskedInput from 'antd-mask-input';
 import { MyCard, ViewportContainer } from '../../components';
-import { standardGutter } from '../../shared/constants/gutter';
 import axios from 'axios';
 import './CallBack.scss';
 
@@ -15,52 +14,46 @@ export const CallBack = () => {
   };
 
   return (
-    <Row
-      className="callback"
-      justify="center"
-      gutter={{ ...standardGutter }}
-    >
-      <Col xs={24} md={14} lg={12} xl={10} xxl={8}>
-        <ViewportContainer>
-          <MyCard>
-            <h2 className="callback__title">Заказать звонок</h2>
-            <Form
-              layout="vertical"
-              onFinish={onSubmit}
+    <div className="callback">
+      <ViewportContainer>
+        <MyCard className="callback__card">
+          <h2 className="callback__title">Заказать звонок</h2>
+          <Form
+            layout="vertical"
+            onFinish={onSubmit}
+          >
+            <Form.Item
+              label="Как к вам обращаться:"
+              name="name"
+              rules={[
+                { required: true, message: 'Пожалуйста, ведите имя!' },
+              ]}
             >
-              <Form.Item
-                label="Как к вам обращаться:"
-                name="name"
-                rules={[
-                  { required: true, message: 'Пожалуйста, ведите имя!' },
-                ]}
-              >
-                <Input />
-              </Form.Item>
-              <Form.Item
-                label="Телефон:"
-                name="phone"
-                rules={[
-                  { required: true, message: 'Пожалуйста, ведите телефон!' },
-                ]}
-              >
-                <MaskedInput
-                  mask="(111) 111-11-11"
-                  prefix="+7"
-                />
-              </Form.Item>
-              <Form.Item label="Email:" name="email">
-                <Input />
-              </Form.Item>
-              <Form.Item noStyle>
-                <Button type="primary" htmlType="submit" block>
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Телефон:"
+              name="phone"
+              rules={[
+                { required: true, message: 'Пожалуйста, ведите телефон!' },
+              ]}
+            >
+              <MaskedInput
+                mask="(111) 111-11-11"
+                prefix="+7"
+              />
+            </Form.Item>
+            <Form.Item label="Email:" name="email">
+              <Input />
+            </Form.Item>
+            <Form.Item noStyle>
+              <Button type="primary" htmlType="submit" block>
                   Отправить
-                </Button>
-              </Form.Item>
-            </Form>
-          </MyCard>
-        </ViewportContainer>
-      </Col>
-    </Row>
+              </Button>
+            </Form.Item>
+          </Form>
+        </MyCard>
+      </ViewportContainer>
+    </div>
   );
 };
