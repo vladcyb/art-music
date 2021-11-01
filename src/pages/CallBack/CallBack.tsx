621869aacbad2d4174ddc6d1daa2f19025998d8f
Vlad Tsibizov <vladtsibizov@gmail.com>
2021-11-01 08:01:27 +0300

    Отправка POST-запроса при заполнении формы обратной связи

--- src/pages/CallBack/CallBack.tsx
+++ src/pages/CallBack/CallBack.tsx
@@ -2,11 +2,16 @@ import { Button, Col, Form, Input, Row } from 'antd';
 import MaskedInput from 'antd-mask-input';
 import { MyCard, ViewportContainer } from '../../components';
 import { standardGutter } from '../../shared/constants/gutter';
+import axios from 'axios';
 import './CallBack.scss';
 
 export const CallBack = () => {
   const onSubmit = (values: any) => {
-    console.log(values);
+    const data = {
+      ...values,
+      phone: `+7 ${values.phone}`,
+    };
+    axios.post('/callMe', data, { withCredentials: true });
   };
 
   return (
@@ -23,22 +28,33 @@ export const CallBack = () => {
               layout="vertical"
               onFinish={onSubmit}
             >
-              <Form.Item label="Как к вам обращаться:">
-                <Input name="name" />
+              <Form.Item
+                label="Как к вам обращаться:"
+                name="name"
+                rules={[
+                  { required: true, message: 'Пожалуйста, ведите имя!' },
+                ]}
+              >
+                <Input />
               </Form.Item>
-              <Form.Item label="Телефон:">
+              <Form.Item
+                label="Телефон:"
+                name="phone"
+                rules={[
+                  { required: true, message: 'Пожалуйста, ведите телефон!' },
+                ]}
+              >
                 <MaskedInput
                   mask="(111) 111-11-11"
-                  name="phone"
                   prefix="+7"
                 />
               </Form.Item>
-              <Form.Item label="Email:">
-                <Input name="email" />
+              <Form.Item label="Email:" name="email">
+                <Input />
               </Form.Item>
               <Form.Item noStyle>
                 <Button type="primary" htmlType="submit" block>
-                Отправить
+                  Отправить
                 </Button>
               </Form.Item>
             </Form>
